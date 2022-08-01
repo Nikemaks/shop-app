638025b2fb186c2deeffdd482c88250fffa9f298
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2022-08-01 22:46:37 +0300

    add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from "./components/pages/history-pages/history.component";
+import { CreateListComponent } from "./components/pages/create-list-pages/create-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CreateListComponent},
+  {path: 'history', component: HistoryComponent},
+  {path: 'create', component: CreateListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/pages/main-page/main-page.component';
+import { CreateListComponent } from './components/pages/create-list-pages/create-list.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HistoryComponent } from "./components/pages/history-pages/history.component";
+import { ListComponent } from './components/pages/create-list-pages/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    CreateListComponent,
+    MenuComponent,
+    HistoryComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
