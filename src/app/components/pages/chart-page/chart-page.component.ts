90f04c2e60f61fa854c6a806ec5e2db93bbc8646
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2022-08-03 12:57:56 +0300

    add chart date

--- src/app/components/pages/chart-page/chart-page.component.ts
+++ src/app/components/pages/chart-page/chart-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartType } from "angular-google-charts";
+import { ListItemsInterface } from "../../../interfaces/list-items";
+import { LocalStorageService, SHOP_ITEMS } from "../../../services/local-storage.service";
 
 @Component({
   selector: 'app-chart-page',
@@ -11,7 +13,7 @@ export class ChartPageComponent implements OnInit {
   chart = {
     title: 'Title',
     type: ChartType.PieChart,
-    data: [ ['London', 8136000],
+    data: [['London', 8136000],
       ['New York', 8538000],
       ['Paris', 2244000],
       ['Berlin', 3470000],
@@ -19,9 +21,34 @@ export class ChartPageComponent implements OnInit {
     columnNames: ['', ''],
     options: {}
   }
-  constructor() { }
+
+  constructor(private localStorageService: LocalStorageService) {
+  }
 
   ngOnInit(): void {
+    const listItems: Array<ListItemsInterface> = this.localStorageService.getStorageItem(SHOP_ITEMS);
+    let finalObj: { [index: string]: Array<ListItemsInterface> } = {}
+    listItems.forEach((purchases) => {
+      if (finalObj[purchases.type]) {
+        finalObj[purchases.type].push(purchases);
+      } else {
+        finalObj[purchases.type] = [purchases];
+      }
+    });
+
+    let chartDate: Array<Array<string | number>> = [];
+    Object.keys(finalObj).forEach(title => {
+      const price = finalObj[title].reduce((memo, item) => {
+        return memo + (item.price * item.count);
+      }, 0);
+
+      chartDate.push([
+        title,
+        price
+      ])
+    })
+
+    this.chart.data = chartDate;
   }
 
 }
